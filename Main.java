a7652405d245d8069ff04644b264034663d0cf76
Abdulrazak <rizeq@Abdulrazaks-MBP.fritz.box>
2023-10-20 22:26:49 +0200

    added a loop

--- Main.java
+++ Main.java
@@ -1,13 +1,23 @@
 import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the number of minutes: ");
-        long minutes = scanner.nextLong();
 
-        CalcYearsAndDays.printYearsDaysSeconds(minutes);
+        while (true) {
+            System.out.print("Enter the number of minutes or 'stop' to exit: ");
+            String input = scanner.next();
+
+            if (input.equalsIgnoreCase("stop")) {
+                break; // Exit the loop if 'stop' is entered
+            }
+
+            try {
+                long minutes = Long.parseLong(input);
+                CalcYearsAndDays.printYearsDaysSeconds(minutes);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid number of minutes.");
+            }
+        }
 
         scanner.close();
     }
